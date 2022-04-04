93ec04a9197baf59fc7809a1a090d7f378325a8c
NecroMoreno <fmoremerc@gmail.com>
2022-04-03 20:18:09 -0400

    Cuarto update falta hacer funcionar la coma y otros botones la logica de las operaciones está operativa

--- calculadora.frontend/assets/js/calculadora.js
+++ calculadora.frontend/assets/js/calculadora.js
@@ -1,119 +1,198 @@
-
 //definiendo las variables
 
 var operandoA;
 var operandoB;
+var operandoAux = "";
 var operacion;
+var bandera = false;
+var bandera2 = false;
 
-function init(){
-    var 
-        operando = document.getElementById("operando"),
-        resultado = document.getElementById("resultado"),
-        resetError = document.getElementById("ceBtn"),
-        reset = document.getElementById("cBtn"),
-        resetLast = document.getElementById("arrowBtn"),
-        divide = document.getElementById("divideBtn"),
-        siete = document.getElementById("sieteBtn"),
-        ocho = document.getElementById("ochoBtn"),
-        nueve = document.getElementById("nueveBtn"),
-        multiplica = document.getElementById("multiplicaBtn"),
-        cuatro = document.getElementById("cuatroBtn"),
-        cinco = document.getElementById("cincoBtn"),
-        seis = document.getElementById("seisBtn"),
-        resta = document.getElementById("restaBtn"),
-        uno = document.getElementById("unoBtn"),
-        dos = document.getElementById("dosBtn"),
-        tres = document.getElementById("tresBtn"),
-        suma = document.getElementById("sumaBtn"),
-        zero = document.getElementById("zeroBtn"),
-        coma = document.getElementById("comaBtn"),
-        igual = document.getElementById("igualBtn");
+//no tengo ni idea para qué hace esto el profe si nunca las usa lol
+function init() {
+  var operando = document.getElementById("operando"),
+    resultado = document.getElementById("resultado"),
+    resetError = document.getElementById("ceBtn"),
+    reset = document.getElementById("cBtn"),
+    resetLast = document.getElementById("arrowBtn"),
+    divide = document.getElementById("divideBtn"),
+    siete = document.getElementById("sieteBtn"),
+    ocho = document.getElementById("ochoBtn"),
+    nueve = document.getElementById("nueveBtn"),
+    multiplica = document.getElementById("multiplicaBtn"),
+    cuatro = document.getElementById("cuatroBtn"),
+    cinco = document.getElementById("cincoBtn"),
+    seis = document.getElementById("seisBtn"),
+    resta = document.getElementById("restaBtn"),
+    uno = document.getElementById("unoBtn"),
+    dos = document.getElementById("dosBtn"),
+    tres = document.getElementById("tresBtn"),
+    suma = document.getElementById("sumaBtn"),
+    zero = document.getElementById("zeroBtn"),
+    coma = document.getElementById("comaBtn"),
+    igual = document.getElementById("igualBtn");
 }
 
-unoBtn.onclick = function(e){
-    operando.textContent = operando.textContent  + "1";
-}
-
-unoBtn.onclick = function(e){
-    operando.textContent = operando.textContent  + "1";
-}
-
-dosBtn.onclick = function(e){
-    operando.textContent = operando.textContent  + "2";
-}
-
-tresBtn.onclick = function(e){
-    operando.textContent = operando.textContent  + "3";
-}
-
-cuatroBtn.onclick = function(e){
-    operando.textContent = operando.textContent  + "4";
-}
-
-cincoBtn.onclick = function(e){
-    operando.textContent = operando.textContent  + "5";
-}
-
-seisBtn.onclick = function(e){
-    operando.textContent = operando.textContent  + "6";
-}
-
-sieteBtn.onclick = function(e){
-    operando.textContent = operando.textContent  + "7";
-}
-
-ochoBtn.onclick = function(e){
-    operando.textContent = operando.textContent  + "8";
-}
-
-nueveBtn.onclick = function(e){
-    operando.textContent = operando.textContent  + "9";
-}
-
-zeroBtn.onclick = function(e){
-    operando.textContent = operando.textContent  + "0";
-}
-
-divideBtn.onclick = function(e){
-    operando.textContent = operando.textContent  + "÷";
-}
-
-multiplicaBtn.onclick = function(e){
-    operando.textContent = operando.textContent  + "×";
-}
-
-restaBtn.onclick = function(e){
-    operando.textContent = operando.textContent  + "-";
-}
-
-sumaBtn.onclick = function(e){
-    operando.textContent = operando.textContent  + "+";
-}
-
-ceBtn.onclick = function(e){
-    resetear();
-}
-
-cBtn.onclick = function(e){
-    resetear();
-}
-
-arrowBtn.onclick = function(e){
-    operando.textContent = operando.textContent  + "+";
-}
-
-function resetear(){
-    resultado.textContent = "";
-    operando.textContent = "";
-    operandoA = 0;
-    operandoB = 0;
-    operacion = "";
+unoBtn.onclick = function () {
+  if (bandera2 == false) {
+    operando.textContent = operando.textContent + "1";
+    if (bandera == true) {
+      operandoAux = operandoAux + "1";
+    }
   }
+};
 
-  function calculo(){
-    
-
-
-
-
+dosBtn.onclick = function () {
+  if (bandera2 == false) {
+    operando.textContent = operando.textContent + "2";
+    if (bandera == true) {
+      operandoAux = operandoAux + "2";
+    }
   }
+};
+
+tresBtn.onclick = function () {
+  if (bandera2 == false) {
+    operando.textContent = operando.textContent + "3";
+    if (bandera == true) {
+      operandoAux = operandoAux + "3";
+    }
+  }
+};
+
+cuatroBtn.onclick = function () {
+  if (bandera2 == false) {
+    operando.textContent = operando.textContent + "4";
+    if (bandera == true) {
+      operandoAux = operandoAux + "4";
+    }
+  }
+};
+
+cincoBtn.onclick = function () {
+  if (bandera2 == false) {
+    operando.textContent = operando.textContent + "5";
+    if (bandera == true) {
+      operandoAux = operandoAux + "5";
+    }
+  }
+};
+
+seisBtn.onclick = function () {
+  if (bandera2 == false) {
+    operando.textContent = operando.textContent + "6";
+    if (bandera == true) {
+      operandoAux = operandoAux + "6";
+    }
+  }
+};
+
+sieteBtn.onclick = function () {
+  if (bandera2 == false) {
+    operando.textContent = operando.textContent + "7";
+    if (bandera == true) {
+      operandoAux = operandoAux + "7";
+    }
+  }
+};
+
+ochoBtn.onclick = function () {
+  if (bandera2 == false) {
+    operando.textContent = operando.textContent + "8";
+    if (bandera == true) {
+      operandoAux = operandoAux + "8";
+    }
+  }
+};
+
+nueveBtn.onclick = function () {
+  if (bandera2 == false) {
+    operando.textContent = operando.textContent + "9";
+    if (bandera == true) {
+      operandoAux = operandoAux + "9";
+    }
+  }
+};
+
+zeroBtn.onclick = function () {
+  if (bandera2 == false) {
+    operando.textContent = operando.textContent + "0";
+    if (bandera == true) {
+      operandoAux = operandoAux + "0";
+    }
+  }
+};
+
+divideBtn.onclick = function () {
+  if (bandera != true) {
+    operandoA = operando.textContent;
+    operando.textContent = operando.textContent + "÷";
+    bandera = true;
+    operacion = "/";
+  }
+};
+
+multiplicaBtn.onclick = function () {
+  if (bandera != true) {
+    operandoA = operando.textContent;
+    operando.textContent = operando.textContent + "×";
+    bandera = true;
+    operacion = "*";
+  }
+};
+
+restaBtn.onclick = function () {
+  if (bandera != true) {
+    operandoA = operando.textContent;
+    operando.textContent = operando.textContent + "-";
+    bandera = true;
+    operacion = "-";
+  }
+};
+
+sumaBtn.onclick = function () {
+  if (bandera != true) {
+    operandoA = operando.textContent;
+    operando.textContent = operando.textContent + "+";
+    bandera = true;
+    operacion = "+";
+  }
+};
+
+ceBtn.onclick = function () {
+  resetear();
+};
+
+cBtn.onclick = function () {
+  resetear();
+};
+
+arrowBtn.onclick = function () {};
+
+function resetear() {
+  resultado.textContent = "";
+  operando.textContent = "";
+  operandoAux = "";
+  operandoA = "";
+  operacion = "";
+  bandera = false;
+  bandera2 = false;
+}
+
+igualBtn.onclick = function () {
+  if (operacion == "/") {
+    var resultadoFinal = parseFloat(operandoA) / parseFloat(operandoAux);
+    resultado.textContent = resultadoFinal;
+    bandera2 = true;
+  } else if (operacion == "*") {
+    var resultadoFinal = parseFloat(operandoA) * parseFloat(operandoAux);
+    resultado.textContent = resultadoFinal;
+    bandera2 = true;
+  } else if (operacion == "-") {
+    var resultadoFinal = parseFloat(operandoA) - parseFloat(operandoAux);
+    resultado.textContent = resultadoFinal;
+  } else if (operacion == "+") {
+    var resultadoFinal = parseFloat(operandoA) + parseFloat(operandoAux);
+    resultado.textContent = resultadoFinal;
+    bandera2 = true;
+  }
+};
